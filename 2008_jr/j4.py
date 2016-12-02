b60ccfc3f96ba1b2d4da69bac8d3f0a749395799
yjs990427 <yjs990427@gmail.com>
2016-12-01 20:07:22 -0500

    I think I solved it prefix_postfix

--- 2008_jr/j4.py
+++ 2008_jr/j4.py
@@ -1,31 +1,59 @@
+def complete(exprlist):
+    operand = 0
+    operator = 0
+    for i in exprlist:
+        if i in "+-":
+            operator += 1
+
+        else:
+            operand += 1
+
+    return operand - 1 == operator
+
+
 def prefix_postfix(prefix):
     prefix = prefix.split()
     index = 0
     operlist = []
     operandlist = []
     postfixlist = []
-    for i in range(len(prefix)):
-        token = prefix[i]
-        if token in "+-":
+    while len(prefix) > 0:
+        token = prefix.pop(0)
+        if token in "+-":                   #when token is operator
             operlist.append(token)
             if len(operandlist) == 1:
                 postfixlist.append(operandlist.pop())
-        
-        else:
-            if len(operandlist) == 2:
-                postfixlist.append(operandlist.pop(0))
-                postfixlist.append(operandlist.pop(0))
-                postfixlist.append(operlist.pop())
-                
+            
+            if prefix[0].isdigit() and prefix[1].isdigit():
+                postfixlist.append(prefix.pop(0))
+                postfixlist.append(prefix.pop(0))
+                postfixlist.append(operlist.pop())              
+
+        else:                               #when token is operand
             operandlist.append(token)
             
-    if len(operandlist) == 1:
-        postfixlist.append(operlist.pop())
-        postfixlist.append(operandlist.pop())
-    
-    for i in operlist:
-        postfixlist.append(i)
+            if len(postfixlist) > 0:
+                if complete(postfixlist):
+                    postfixlist.append(operandlist.pop())
+
+                elif not complete(postfixlist):
+                    postfixlist.append(operlist.pop())
+                    postfixlist.append(operandlist.pop())
+
+    if len(operandlist) != 0 and len(operlist) != 0:
+        if complete(postfixlist):
+            for i in range(operandlist):
+                postfixlist.append(operandlist.pop())
+                postfixlist.append(operlist.pop())
+                
+            
+    if len(operandlist) == 0 and len(operlist) != 0:
+        for i in range(len(operlist)):
+            postfixlist.append(operlist.pop())
+
         
     return " ".join(postfixlist)
     
-print(prefix_postfix("- - 3 + 2 1 9"))
+print(prefix_postfix("+ - 5 + 4 3 2"))
+
+print(prefix_postfix("+ - + 3 2 1 2"))
