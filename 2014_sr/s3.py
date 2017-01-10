ec72685304b8da39e3aebff6ff1cc936796c77f7
yjs990427 <yjs990427@gmail.com>
2017-01-10 15:18:27 +0000

    s3 basic file operation commit

---
+++ 2014_sr/s3.py
@@ -0,0 +1,13 @@
+# the Geneva confection
+# basic file input
+with open('s3.in', 'r') as f:
+    for i in range(int(f.readline())):
+        numlist = []                                # first number to be treated is at the end
+        for i in range(int(f.readline())):
+            numlist.append(int(f.readline())) 
+        print(numlist) 
+        # logic start here 
+        
+        
+
+
