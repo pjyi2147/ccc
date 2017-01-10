3ac65d2046f440cf28869516d886f823beb8a8af
yjs990427 <yjs990427@gmail.com>
2017-01-10 15:26:40 +0000

    support of cars less than 3

--- 2014_sr/s3.py
+++ 2014_sr/s3.py
@@ -2,12 +2,18 @@
 # basic file input
 with open('s3.in', 'r') as f:
     for i in range(int(f.readline())):
-        numlist = []                                # first number to be treated is at the end
-        for i in range(int(f.readline())):
+        # this list is listed backwards
+        numlist = []      
+        k = int( f.readline())
+        if k < 4:
+            print('Y')
+            break 
+
+        for i in range(k):
             numlist.append(int(f.readline())) 
         print(numlist) 
         # logic start here 
         
-        
+       
 
 
