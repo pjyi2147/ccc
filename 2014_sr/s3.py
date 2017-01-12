e6597ae7644a19ec2c62304d33c2b8d34a4c0ced
yjs990427 <yjs990427@gmail.com>
2017-01-11 19:33:09 -0500

    not finished s3.py

--- 2014_sr/s3.py
+++ 2014_sr/s3.py
@@ -1,60 +1,41 @@
 # the Geneva confection
 # basic file input
-with open('s3.in', 'r') as f:
-    for i in range(int(f.readline())):
+import os
+import sys 
+with open(os.path.join(sys.path[0], 's3.5.in'), 'r') as f:
+    entirefile = f.readlines()
+    p = 0
+    t = int(entirefile[p]) 
+    p = p + 1
+    for i in range(int(t)):
         # this list is listed backwards
-        numlist = []      
-        k = int( f.readline())
-        if k < 4:
-            print('Y')
-            for i in range(k):
-               f.readline()
-            continue 
+        numlist = [] 
+        n = int(entirefile[p])  
+        p = p + 1     
+        for i in range(n):
+            numlist.append(int(entirefile[p]))
+            p = p + 1
 
-        for i in range(k):
-            numlist.append(int(f.readline())) 
-        
         branch = []
         lake = []
         # logic start here 
-        fail = False
-        while len(numlist) > 0:        #this only 0
-            num = numlist.pop()
-
-            if num == 1: 
-                lake.append(num) 
-
+        fail = True
+        while fail and len(numlist) > 0: 
+            nextCar = 1  
+            if len(numlist) > 0 and nextCar == numlist[-1]:
+                numlist.pop()
+                nextCar = nextCar + 1
             
-            elif len(lake) > 0:
+            elif len(branch) > 0 and nextCar == branch[-1]:
+                branch.pop()
+                nextCar = nextCar + 1
+            
+            elif len(numlist) > 0:
+                branch.append(numlist.pop())
+            
+            if len(numlist) == 0 and len(branch) == 0:
+                fail = False
 
-                if num == lake[-1]:
-                    lake.append(num)
-                
-                while len(branch) > 0:
-                    if branch[-1] == lake[-1]:
-                        lake.append(branch.pop())
-
-                    elif num != lake[-1]: 
-                        branch.append(num)
-                    
-                    else: 
-                        break
-               
-            else: 
-                branch.append(num)
-
-        while len(branch) > 0:
-            if branch[-1] == lake[-1]:
-                lake.append(branch.pop())
-     
-
-        if len(branch) != 0:
-            fail = True
-        
-       
-       
-
-        
         if not fail:
             print('Y')
 
