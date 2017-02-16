37009046186560eb45b42f96a1ad343c26dcab87
Jeongseop Yi <yjs990427@gmail.com>
2017-02-16 17:56:21 -0500

    solved it

--- 2010_sr/s2.py
+++ 2010_sr/s2.py
@@ -1,13 +1,23 @@
+import sys
+import os
 def Solution():
-    f = open('S2.in' ,'r')
+    f = open(os.path.join(sys.path[0], 's2.in'),'r')
     a = f.readline()
     lst = {}
     for i in range(int(a)):
         string = f.readline().split()
         x, y = string[0], string[1]
-        lst[x:y]
+        lst[x] = y
 
-    print(lst)
+    answer = ''
+    binary = f.readline()
+    while len(binary) > 0:
+        for k, v in lst.items():
+            if binary.startswith(v):
+                answer += k
+                binary = binary[len(v):]
+                break
+                
+    print(answer)
 
-    
 Solution()
